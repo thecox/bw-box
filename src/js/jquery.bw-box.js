1a3829c257e976bc1f842e2a41cb1c69617dc0f6
Andrew Cox <andrew.cox@PHQ-ACOX-MBPR.local>
2015-12-29 13:54:35 -0700

    Comments and styling updates

--- src/js/jquery.bw-box.js
+++ src/js/jquery.bw-box.js
@@ -1,15 +1,32 @@
+/**
+ *  Copyright 2015 Andrew Cox
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 (function($) {
   'use strict';
 
   $.fn.bwBox = function( options ) {
 
-    // Default settings
+    // Plugin options / default settings
     var settings = $.extend({
       selectorType: 'data', // data or element
       elementSelectors: null,
       complete: null
     }, options);
 
+    // Return jQuery object for chaining
     return this.each(function(index) {
       // Global variable declarations
       // +++++ NEED TO ADD VARIABLE COMMENTS +++++
